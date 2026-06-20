6be089817e081fac5930f431a77fec4c2f0b0fbe
Jhose <jhosepjamil@gmail.com>
2026-06-19 21:54:19 -0500

    fix: update file tree, remove folders that follow a clean architecture

--- src/features/reports/application/use-cases/GetConsumptionReportUseCase.js
+++
@@ -1,9 +0,0 @@
-export class GetConsumptionReportUseCase {
-  constructor(reportRepository) {
-    this.reportRepository = reportRepository;
-  }
-
-  async execute() {
-    return await this.reportRepository.getConsumptionSummary();
-  }
-}
